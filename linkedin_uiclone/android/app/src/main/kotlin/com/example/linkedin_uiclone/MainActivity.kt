ec5c808d88ec739c5938cac08991d6a1c92b8da0
Olasoji Oluwadamilola <dami.osoj@gmail.com>
2022-02-08 22:21:56 +0100

    First Commit

---
+++ linkedin_uiclone/android/app/src/main/kotlin/com/example/linkedin_uiclone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.linkedin_uiclone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ linkedin_uiclone/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "LinkedIn Clone",
+      home: SplashScreen(),
+    );
+  }
+}
